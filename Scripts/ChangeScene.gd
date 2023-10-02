da28171e93a6db972727fdd1fca54e1e4fd90120
Rodrigo Montenegro <rodrigo.montenegro.do@gmail.com>
2023-10-02 15:07:44 -0300

    fix

--- Scripts/ChangeScene.gd
+++ Scripts/ChangeScene.gd
@@ -19,3 +19,12 @@ func _ready():
 func _on_Button_pressed():
 	get_tree().change_scene(ChangeSceneTo)
 	pass # Replace with function body.
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene(ChangeSceneTo)
+	pass # Replace with function body.
+
+
+func _on_Button3_pressed():
+	get_tree().quit()
